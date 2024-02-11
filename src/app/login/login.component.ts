b6cd519abe22a77769c0a140e440bf85f3a15a55
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-02-11 07:46:30 +0100

    Adding click event on Login Page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  username: string = 'John Doe'; // Default Value
+  password: string = '1234'; // Default Value
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  handleLogin() {
+    console.log(this.username);
+  }
 }
